936fb7f6aaa9018c5e8d44ddca970509009ddca3
David Harris <davidharris006@gmail.com>
2019-06-20 18:06:02 -0700

    taking out button for heroku google sign in

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,15 +1,19 @@
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
-var passport = require('passport')
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 
+// Use the GoogleStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a token, tokenSecret, and Google profile), and
+//   invoke a callback with a user object.
 passport.use(new GoogleStrategy({
-    clientID: "66709955163-gn4qb9posephajpv0m5vdres6g3e29fr.apps.googleusercontent.com",
-    clientSecret: "znnOzafewcu83s3uYbNGUmTb",
+    consumerKey:"66709955163-gn4qb9posephajpv0m5vdres6g3e29fr.apps.googleusercontent.com",
+    consumerSecret: "znnOzafewcu83s3uYbNGUmTb",
     callbackURL: "http://www.example.com/auth/google/callback"
   },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+  function(token, tokenSecret, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
   }
 ));
 
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,13 +3,16 @@ var passport = require("../config/passport")
 module.exports = function(app) {
 
     app.get('/auth/google',
-    passport.authenticate('google', { scope: ['profile'] }));
+    passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' }))
   
+  // GET /auth/google/callback
+  //   Use passport.authenticate() as route middleware to authenticate the
+  //   request.  If authentication fails, the user will be redirected back to the
+  //   login page.  Otherwise, the primary route function function will be called,
+  //   which, in this example, will redirect the user to the home page.
   app.get('/auth/google/callback', 
     passport.authenticate('google', { failureRedirect: '/login' }),
     function(req, res) {
-      // Successful authentication, redirect home.
       res.redirect('/');
-    });
-    
+    })
 };
